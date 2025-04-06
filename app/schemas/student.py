0b12d24353aa071a58669b70596020797e462049
Rakesh Kumar Sahoo <rakeshsahoo16@gmail.com>
2025-04-06 22:47:26 +0530

    corrected the student class code and student response model with student base model

--- app/schemas/student.py
+++ app/schemas/student.py
@@ -3,15 +3,13 @@ from pydantic import BaseModel, field_validator
 
 class StudentGet(BaseModel):
     """StudentGet model definition."""
-    # student_id: int
     name: str
-    # age: int
     email: str | None = None
     roll_number: str | None = None
 
     class Config:
         orm_mode = True
-        # arbitrary_types_allowed = True
+
 
 class StudentResponse(StudentGet):
     id: int
